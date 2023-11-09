98b776146a33a198930bd01464db2cba8f8a389f
blezz-tech <markus.jenya04@yandex.ru>
2023-11-10 00:23:26 +0300

    update

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-23.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, ... }: {
--- home/programs/common.nix
+++ home/programs/common.nix
@@ -8,7 +8,7 @@
   home.packages = with pkgs; [
     # LaTeX
     # texlive.combined.scheme-full
-    pdf2svg
+    # pdf2svg
 
     # Torrent
     qbittorrent
@@ -21,7 +21,7 @@
     vlc
 
     # Альтернатива Postman'у
-    insomnia
+    # insomnia
 
     # 3D редакторы
     # blender
@@ -37,9 +37,9 @@
     # sigil
 
     # Графические редакторы
-    gimp
-    inkscape
-    krita
+    # gimp
+    # inkscape
+    # krita
     # drawio
 
     # Текстовые редакторы
@@ -49,11 +49,11 @@
     neovim
 
     # Браузеры
-    vivaldi
+    # vivaldi
     firefox
 
     # База знаний
-    obsidian
+    # obsidian
 
     # Запись видео и потоковое вещание
     # obs-studio
@@ -70,10 +70,10 @@
     # sqlitebrowser
 
     ## Android
-    android-studio
+    # android-studio
 
     ## Игровые движки
-    godot_4
+    # godot_4
 
     ## haskell
     # stack
@@ -92,7 +92,7 @@
     ## Rust 
     # cargo
     # rustc
-    rustfmt
+    # rustfmt
     rustup
 
     ## C
