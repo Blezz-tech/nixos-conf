3771f65e7429d3457b0cf4cc145d0cf8488244eb
blezz-tech <markus.jenya04@yandex.ru>
2023-11-10 01:22:06 +0300

    update

--- flake.nix
+++ flake.nix
@@ -8,39 +8,41 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, ... }: {
-    nixosConfigurations = {
-      "default" = nixpkgs.lib.nixosSystem rec {
-        system = "x86_64-linux";
-        specialArgs = {
-          pkgs-unstable = import nixpkgs-unstable {
-            system = system;
-            config.allowUnfree = true;
-          };
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
+    let
+    system = "x86_64-linux";
+      specialArgs = {
+        pkgs-unstable = import nixpkgs-unstable {
+          inherit system;
+          config.allowUnfree = true;
         };
-        modules = [
-          ./hosts/laptop
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-
-            home-manager.users.jenya = import ./home;
-          }
-        ];
       };
-      "base" = nixpkgs.lib.nixosSystem rec {
-        system = "x86_64-linux";
-        specialArgs = {
-          pkgs-unstable = import nixpkgs-unstable {
-            system = system;
-            config.allowUnfree = true;
-          };
+    in
+    {
+      nixosConfigurations = {
+        "default" = nixpkgs.lib.nixosSystem {
+          inherit system;
+          inherit specialArgs;
+          modules = [
+            ./hosts/laptop
+            home-manager.nixosModules.home-manager {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+
+                users.jenya = import ./home;
+                extraSpecialArgs = specialArgs;
+              };
+            }
+          ];
         };
-        modules = [
-          ./hosts/laptop
-        ];
+        "base" = nixpkgs.lib.nixosSystem {
+          inherit system;
+          inherit specialArgs;
+          modules = [
+            ./hosts/laptop
+          ];
+        };
       };
     };
-  };
 }
