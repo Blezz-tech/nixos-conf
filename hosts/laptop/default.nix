201a97145ace20e1ae7d0cd0a34a5685e2b71fb7
blezz-tech <markus.jenya04@yandex.ru>
2023-12-14 17:10:02 +0300

    update locale

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -12,7 +12,7 @@
   nixpkgs.config.permittedInsecurePackages = [
     "electron-25.9.0"
   ];
-  
+
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
     trusted-users = [ "root" "jenya" ];
@@ -56,15 +56,20 @@
     defaultLocale = "ru_RU.UTF-8";
 
     extraLocaleSettings = {
-      LC_ADDRESS = "ru_RU.UTF-8";
-      LC_IDENTIFICATION = "ru_RU.UTF-8";
-      LC_MEASUREMENT = "ru_RU.UTF-8";
-      LC_MONETARY = "ru_RU.UTF-8";
-      LC_NAME = "ru_RU.UTF-8";
+      LANG = "ru_RU.UTF-8";
+      LC_CTYPE = "ru_RU.UTF-8";
       LC_NUMERIC = "ru_RU.UTF-8";
-      LC_PAPER = "ru_RU.UTF-8";
-      LC_TELEPHONE = "ru_RU.UTF-8";
       LC_TIME = "ru_RU.UTF-8";
+      LC_COLLATE = "ru_RU.UTF-8";
+      LC_MONETARY = "ru_RU.UTF-8";
+      LC_MESSAGES = "ru_RU.UTF-8";
+      LC_PAPER = "ru_RU.UTF-8";
+      LC_NAME = "ru_RU.UTF-8";
+      LC_ADDRESS = "ru_RU.UTF-8";
+      LC_TELEPHONE = "ru_RU.UTF-8";
+      LC_MEASUREMENT = "ru_RU.UTF-8";
+      LC_IDENTIFICATION = "ru_RU.UTF-8";
+      LC_ALL = "ru_RU.UTF-8";
     };
   };
 
