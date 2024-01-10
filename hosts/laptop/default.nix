eb6c7c1fb234144199aa5a7e906ba77086df0c2d
blezz-tech <markus.jenya04@yandex.ru>
2024-01-10 14:45:22 +0300

    delete msg

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -137,7 +137,7 @@
     gitea = {
       enable = true;
       lfs.enable = true;
-      
+
       settings = {
         server = {
           # PROTOCOL = "https";
@@ -274,12 +274,5 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # networking.firewall.enable = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
-
+  system.stateVersion = "23.05";
 }
