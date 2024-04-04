6344cead6206a2ce708c3b0d96803f95fa11a9ab
blezz-tech <markus.jenya04@yandex.ru>
2024-04-04 09:58:48 +0300

    convert libsForQt5 to kdePackages

--- home/programs/common.nix
+++ home/programs/common.nix
@@ -66,18 +66,18 @@
     # openspades
     airshipper
     
-    libsForQt5.kolf
-    libsForQt5.kmahjongg
-    libsForQt5.knights
-    libsForQt5.knavalbattle
-    libsForQt5.kmines
-    libsForQt5.picmi
-    libsForQt5.ksudoku
-    libsForQt5.kpat
+    kdePackages.kolf
+    kdePackages.kmahjongg
+    kdePackages.knights
+    kdePackages.knavalbattle
+    kdePackages.kmines
+    kdePackages.picmi
+    kdePackages.ksudoku
+    kdePackages.kpat
 
-    # libsForQt5.ksirk
-    # libsForQt5.kajongg
-    # libsForQt5.kwordquiz
+    # kdePackages.ksirk
+    # kdePackages.kajongg
+    # kdePackages.kwordquiz
 
     # На сортировку
     python3
@@ -116,7 +116,7 @@
     # ocrmypdf
     # stack
     nheko
-    libsForQt5.neochat
+    kdePackages.neochat
     fractal
     tor-browser
     # kalzium
