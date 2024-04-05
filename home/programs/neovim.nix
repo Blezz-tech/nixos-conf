0283f17910a8de2a9891e6e75eaf0dcad06b78d4
blezz-tech <markus.jenya04@yandex.ru>
2024-04-05 17:55:49 +0300

    update nixvim config

--- home/programs/neovim.nix
+++ home/programs/neovim.nix
@@ -23,7 +23,30 @@
     vimAlias = true;
 
     colorschemes.gruvbox.enable = true;
-    plugins.lightline.enable = true;
+
+    plugins = {
+      # lightline.enable = true;
+      barbar.enable = true;
+      gitsigns.enable = true;
+      nvim-autopairs.enable = true;
+      nvim-colorizer.enable = true;
+      fzf-lua.enable = true;
+      lsp = {
+        enable = true;
+      };
+      # lsp.servers.efm = {
+      #   enable = true;
+      # };
+      # lsp-format = {
+      #   enable = true;
+      #   lspServersToEnable = [ "efm" ];
+      # };
+      # efmls-configs.enable = true;
+      neo-tree = {
+        enable = true;
+      };
+      telescope.enable = true;
+    };
   };
 
 }
