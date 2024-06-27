eb50e0b523af2834c384f3ecad66e2fe05f34884
blezz-tech <markus.jenya04@yandex.ru>
2024-06-27 18:20:20 +0300

    enable steam gamescopeSession

--- nixos/default.nix
+++ nixos/default.nix
@@ -132,6 +132,7 @@
 
   programs.steam = {
     enable = true;
+    gamescopeSession.enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   };
