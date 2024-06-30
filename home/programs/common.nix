4f9ae2646382d66418379fc9125e11feefff8646
blezz-tech <markus.jenya04@yandex.ru>
2024-06-30 19:36:28 +0300

    return flag for vivaldi

--- home/programs/common.nix
+++ home/programs/common.nix
@@ -8,7 +8,7 @@
     enable = true;
     package = pkgs.vivaldi;
     commandLineArgs = [
-      # "--disable-features=AllowQt"
+      "--disable-features=AllowQt"
       "--enable-blink-features=MiddleClickAutoscroll"
     ];
   };
