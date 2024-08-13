71fd34f5fb42fe5fff1e0a1809900a46254d3cb2
blezz-tech <markus.jenya04@yandex.ru>
2024-08-14 00:58:02 +0300

    Отключение поддержки qt6 в vivaldi

--- home/programs/common.nix
+++ home/programs/common.nix
@@ -12,20 +12,29 @@
     };
   };
 
+  # programs.chromium = {
+  #   enable = true;
+  #   package = (pkgs.vivaldi.overrideAttrs (oldAttrs: {
+  #     dontWrapQtApps = false;
+  #     dontPatchELF = true;
+  #     nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ pkgs.kdePackages.wrapQtAppsHook ];
+  #   }));
+  #   commandLineArgs = [
+  #     # "--disable-features=AllowQt"
+  #     "--enable-blink-features=MiddleClickAutoscroll"
+
+  #     "--enable-features=VaapiVideoDecodeLinuxGL"
+  #     "--ignore-gpu-blocklist"
+  #     "--enable-zero-copy"
+  #   ];
+  # };
+
   programs.chromium = {
     enable = true;
-    package = (pkgs.vivaldi.overrideAttrs (oldAttrs: {
-      dontWrapQtApps = false;
-      dontPatchELF = true;
-      nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ pkgs.kdePackages.wrapQtAppsHook ];
-    }));
+    package = pkgs.vivaldi;
     commandLineArgs = [
-      # "--disable-features=AllowQt"
+      "--disable-features=AllowQt"
       "--enable-blink-features=MiddleClickAutoscroll"
-
-      "--enable-features=VaapiVideoDecodeLinuxGL"
-      "--ignore-gpu-blocklist"
-      "--enable-zero-copy"
     ];
   };
 
