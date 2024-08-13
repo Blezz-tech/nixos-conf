c812647a2ff140b276257311a6975c546b1f022e
blezz-tech <markus.jenya04@yandex.ru>
2024-08-14 00:59:28 +0300

    Включение GPG агента. отключение openssh

--- nixos/default.nix
+++ nixos/default.nix
@@ -232,23 +232,23 @@
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    ports = [ 22 ];
-    settings.PasswordAuthentication = false;
-    settings.KbdInteractiveAuthentication = false;
-  };
+  # services.openssh = {
+  #   enable = true;
+  #   ports = [ 22 ];
+  #   settings.PasswordAuthentication = false;
+  #   settings.KbdInteractiveAuthentication = false;
+  # };
 
-  programs.ssh.startAgent = true;
+  # programs.ssh.startAgent = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ 80 443 22 ];
