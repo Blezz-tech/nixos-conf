14bc94cd8c8b0ccea86eed8c072186e33b8cdffb
blezz-tech <markus.jenya04@yandex.ru>
2024-08-28 13:31:44 +0300

    add redis service for QSoft

--- nixos/default.nix
+++ nixos/default.nix
@@ -5,6 +5,10 @@
   services.mysql.enable = true;
   services.mysql.package = pkgs.mysql84;
 
+  services.redis.servers."" = {
+    enable = true;
+  };
+
   programs.npm.enable = true;
 
   programs.nix-ld.enable = true;
