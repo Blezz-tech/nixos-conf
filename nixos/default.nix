951f28fa0dc6ef4a384a7342e52b24c297760bc6
blezz-tech <markus.jenya04@yandex.ru>
2025-12-21 12:33:55 +0300

    add printing

--- nixos/default.nix
+++ nixos/default.nix
@@ -248,7 +248,38 @@
   };
 
   # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  # services.avahi = {
+  #   enable = true;
+  #   nssmdns4 = true;
+  #   openFirewall = true;
+  # };
+
+  services.printing = {
+    enable = true;
+    drivers = [
+      pkgs.foomatic-db
+      pkgs.ghostscript
+      pkgs.foomatic-filters
+
+      pkgs.gutenprint # — Drivers for many different printers from many different vendors.
+      pkgs.gutenprintBin # — Additional, binary-only drivers for some printers.
+      pkgs.hplip # — Drivers for HP printers.
+      pkgs.hplipWithPlugin # — Drivers for HP printers, with the proprietary plugin. Use NIXPKGS_ALLOW_UNFREE=1 nix-shell -p hplipWithPlugin --run 'sudo -E hp-setup' to add the printer, regular CUPS UI doesn't seem to work.
+      pkgs.postscript-lexmark # — Postscript drivers for Lexmark
+      # pkgs.samsung-unified-linux-driver # — Proprietary Samsung Drivers
+      # pkgs.splix # — Drivers for printers supporting SPL (Samsung Printer Language).
+      # pkgs.brlaser # — Drivers for some Brother printers
+      # pkgs.brgenml1lpr #  — Generic drivers for more Brother printers [1]
+      # pkgs.brgenml1cupswrapper  # — Generic drivers for more Brother printers [1]
+      pkgs.cnijfilter2 # — Drivers for some Canon Pixma devices (Proprietary driver)
+	
+      pkgs.cups-filters
+      pkgs.cups-browsed
+    ];
+  };
+
+  services.system-config-printer.enable = true;
+  programs.system-config-printer.enable = true;
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
