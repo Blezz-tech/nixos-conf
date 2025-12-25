f14a31e443237d17fc17ef24dadd7c8202d05508
blezz-tech <markus.jenya04@yandex.ru>
2025-12-25 20:53:22 +0300

    disable

--- home/shell/bash.nix
+++ home/shell/bash.nix
@@ -2,10 +2,10 @@
   programs.bash = {
     enable = true;
     enableCompletion = true;
-    # TODO add your cusotm bashrc here
-    bashrcExtra = ''
-      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
-    '';
+    # # TODO add your cusotm bashrc here
+    # bashrcExtra = ''
+    #   export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+    # '';
 
   };
 
