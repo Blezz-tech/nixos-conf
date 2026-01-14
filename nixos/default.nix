6e59d06a9b1cfea4e07179e56e1f903556e4b558
blezz-tech <markus.jenya04@yandex.ru>
2026-01-15 00:38:10 +0300

    dsiable error drivers

--- nixos/default.nix
+++ nixos/default.nix
@@ -263,7 +263,6 @@
 
       pkgs.gutenprint # — Drivers for many different printers from many different vendors.
       pkgs.gutenprintBin # — Additional, binary-only drivers for some printers.
-      pkgs.hplip # — Drivers for HP printers.
       pkgs.hplipWithPlugin # — Drivers for HP printers, with the proprietary plugin. Use NIXPKGS_ALLOW_UNFREE=1 nix-shell -p hplipWithPlugin --run 'sudo -E hp-setup' to add the printer, regular CUPS UI doesn't seem to work.
       pkgs.postscript-lexmark # — Postscript drivers for Lexmark
       # pkgs.samsung-unified-linux-driver # — Proprietary Samsung Drivers
@@ -343,8 +342,6 @@
     keepassxc
     parted
     kdePackages.yakuake
-
-    hplip
 
     home-manager
 
